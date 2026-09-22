b3eb407944c5905d05caa1d44901ee1f8d9c9731
koushikmondal22 <koumon22@gmail.com>
2026-09-22 18:27:53 +0530

    Update grade_calculator.py
    Add pass fail result

--- grade_calculator.py
+++ grade_calculator.py
@@ -9,6 +9,11 @@ statistics = float(input("Enter Statistics marks: "))
 total = math + python + english + computer + statistics
 percentage = total / 5
 
+if percentage >= 40:
+    result = "PASS"
+else:
+    result = "FAIL"
+
 if percentage >= 90:
     grade = "A+"
 elif percentage >= 80:
@@ -27,3 +32,4 @@ print("Student:", name)
 print("Total Marks:", total, "/ 500")
 print("Percentage:", percentage, "%")
 print("Grade:", grade)
+print("Result:", result)
